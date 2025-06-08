687b6f0797e251f0694aa40a6815cdf6bb626f14
Alwin Rex A <alwinrex95@gmail.com>
2025-06-08 13:27:30 +0530

    Task1

---
+++ Task-1/task-1.java
@@ -0,0 +1,21 @@
+package Task1;
+
+public class task1 {
+
+	public static void main(String[] args) {
+		int[] number=new int[6];
+		number[0]=1;
+		number[1]=2;
+		number[2]=3;
+		number[3]=4;
+		number[4]=5;
+		number[5]=6;
+
+
+//	Reverse number
+		for (int i=number.length-1;i>=0 ;i--) {
+		        System.out.println(number[i]);
+		}
+	}	
+
+	}
