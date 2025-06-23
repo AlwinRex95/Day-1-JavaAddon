cc931e68304f27c2df0166bfc273ff2f3025c068
Alwin Rex A <alwinrex95@gmail.com>
2025-06-23 20:30:21 +0530

    Add files via upload

---
+++ Day-1 Tasks/Task1.java
@@ -0,0 +1,14 @@
+package Day1Tasks;
+
+public class Task1 {
+
+		public static void main(String[] args) {
+			for(int i=1; i<=100;i++) {
+				if (i % 2 == 0) {
+					//sum = i+i;
+					System.out.println(i);
+				}
+	}
+		}
+	}
+
---
+++ Day-1 Tasks/Task2.java
@@ -0,0 +1,19 @@
+package Day1Tasks;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		 double principal = 1000; // principal amount
+	        double rate = 5;         // interest rate
+	        double time = 2;         // time in years
+
+	        double interest = 0;
+
+	        // Calculate interest only if values are positive
+	        if (principal > 0 && rate > 0 && time > 0) {
+	            interest = (principal * rate * time) / 100;
+	            System.out.println("Simple Interest is: " + interest);
+	        }
+	}
+
+}
