b10a0d8eb1468d860f75cbb889ca7df874dfcc53
Alwin Rex A <alwinrex95@gmail.com>
2025-06-23 20:15:30 +0530

    Add files via upload

---
+++ Day-2 Tasks/Task1.java
@@ -0,0 +1,22 @@
+package Day2Tasks;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		int[] number=new int[6];
+		number[0]=1;
+		number[1]=2;
+		number[2]=3;
+		number[3]=4;
+		number[4]=5;
+		number[5]=6;
+		
+		for(int i=0; i<=number.length-1;i++) {
+			System.out.println(number[i]);	
+		}
+	//	Reverse number
+	//	for (int i=number.length-1;i>=0 ;i--) {
+		//        System.out.println(number[i]);
+		//}
+	}	
+	}
